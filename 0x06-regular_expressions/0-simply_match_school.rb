0f3ab934b745d36c147a81aa8f46d9c025c86ead
FranQ <frankchristopherquispe@gmail.com>
2022-06-23 09:52:12 +0000

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
+# Simply matching School
+# ------------------------
+# Ruby script that accepts one argument and pass it to a regular expression.
+# The regular expression match School.
+
 puts ARGV[0].scan(/School/).join
